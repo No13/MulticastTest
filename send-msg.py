716f4efa44ad9f3ebcd725804917a48b63c82275
Eric <git@frotmail.nl>
2024-04-19 11:29:29 +0200

    Beide py3

--- send-msg.py
+++ send-msg.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 
 import socket
 import struct
@@ -7,5 +7,5 @@ MCAST_GRP = '234.5.6.2'
 MCAST_PORT = 6666
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)
 sock.setsockopt(socket.IPPROTO_IP, socket.IP_MULTICAST_TTL, 3)
-sock.sendto('Hello World!', (MCAST_GRP, MCAST_PORT))
+sock.sendto(b'Hello World!', (MCAST_GRP, MCAST_PORT))
 
